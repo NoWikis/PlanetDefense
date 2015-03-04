f64a5975fe8d10f3310879b67f104e22d175e8c5
jc <jcorkery@umich.edu>
2015-03-04 16:05:14 -0500

    Added asteroid mesh generation and example spawning

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public float spawnCycle = 1f;
+	public float[] masses;
+	public GameObject asteroidPrefab;
+
+
+	float time = 0;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		if (time > spawnCycle) {
+			GameObject o = (GameObject)Instantiate (asteroidPrefab);
+			o.transform.position = transform.position;
+			o.GetComponent<AsteroidBehavior>().mass = getRandomMass();
+			o.GetComponent<AsteroidPhysics>().initialVelocity = new Vector3(
+				Random.value*3 - 1.5f, Random.value*3 - 1.5f, 0);
+			time = 0f;
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	float getRandomMass() {
+		if (masses.Length == 0) return 100f;
+
+		return masses[(int) Random.Range (0, masses.Length)];
+
+	}
+}
