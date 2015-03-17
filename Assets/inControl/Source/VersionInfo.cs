bc5707b57c72f5d7904deb17785d08b78006184c
jcorks <jcorkery@umich.edu>
2015-03-16 22:31:28 -0400

    Modified asteroid prefab

--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 547
+                Build = 559
 			};
 		}
 		public static VersionInfo UnityVersion()
