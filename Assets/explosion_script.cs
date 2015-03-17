85391f2c9ad07cf3545ed69a2536833cdd8365d9
lee-CS <john.flo.lee@gmail.com>
2015-03-16 23:39:54 -0400

    Explosion fix

---
+++ Assets/explosion_script.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion_script : MonoBehaviour {
+
+	public float expansion_rate = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.localScale.x <= 427f)
+			transform.localScale += new Vector3(expansion_rate, expansion_rate, 0);
+	
+	}
+
+	void OnCollisionEnter(Collision c){
+		Destroy (c.gameObject);
+	}
+}
--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 559
+                Build = 591
 			};
 		}
 		public static VersionInfo UnityVersion()
