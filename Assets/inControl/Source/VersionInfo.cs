29f81eba5d4ecd2218d738d09ae979b4e505c4a2
lee-CS <john.flo.lee@gmail.com>
2015-03-18 16:05:45 -0400

    fixed

--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 801
+                Build = 807
 			};
 		}
 		public static VersionInfo UnityVersion()
