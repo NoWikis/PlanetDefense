70fb8f7f2ceb765418c989716e1b5809e741a87e
lee-CS <john.flo.lee@gmail.com>
2015-03-24 05:09:01 -0400

    first arc done

--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 863
+                Build = 989
 			};
 		}
 		public static VersionInfo UnityVersion()
