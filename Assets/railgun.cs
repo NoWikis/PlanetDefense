aaabb0209f47dff1c5c201bd876d53ea83c354ec
patrshin <patrshin@umich.edu>
2015-03-24 23:05:15 -0400

    started trying laser

---
+++ Assets/railgun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class railgun : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 expanded = transform.localScale;
+		expanded.x += 0.1f;
+		transform.localScale = expanded;
+		Vector3 movement = transform.localPosition;
+		movement.x += 0.1f;
+		transform.localPosition = movement;
+	}
+}
