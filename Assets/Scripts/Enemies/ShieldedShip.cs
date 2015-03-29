c15449ab059429cccec442806c80bb393b4e7e38
jcorks <jcorkery@umich.edu>
2015-03-28 21:54:03 -0400

    Added shield ship

---
+++ Assets/Scripts/Enemies/ShieldedShip.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldedShip : MonoBehaviour {
+
+	public int numShipsPerWave = 2;
+	public float spawnCycle	= 10f;
+	
+
+
+
+
+	public GameObject turret;
+	Health health;
+
+	int count = 0;
+
+
+	// Use this for initialization
+	void Start () {
+		health = GetComponent<Health> ();
+		health.init (100, 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void SpawnShips() {
+
+
+	}
+
+
+
+
+
+}
---
+++ Assets/Scripts/Enemies/ShieldedShipShield.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldedShipShield : MonoBehaviour {
+
+	public GameObject noEffect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnCollisionEnter(Collision c) {
+
+		if (c.gameObject.tag == "Proj_P1" ||
+		    c.gameObject.tag == "Proj_P2") {
+
+			Debug.Log ("What");
+
+			Destroy(c.gameObject);
+			GameObject o = (GameObject)Instantiate (noEffect);
+			o.transform.position = c.gameObject.transform.position;
+
+		}
+
+	}
+}
