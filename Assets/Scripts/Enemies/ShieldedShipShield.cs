9389dcca85a9b824f53110b644e0ace4abd05c2e
jcorks <jcorkery@umich.edu>
2015-03-29 10:49:51 -0400

    Added shielded ship tag (just committing to avoid conflicts with tag manager

--- Assets/Scripts/Enemies/ShieldedShipShield.cs
+++ Assets/Scripts/Enemies/ShieldedShipShield.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class ShieldedShipShield : MonoBehaviour {
 
 	public GameObject noEffect;
+	public float oscillationRate = .3f;
+	GameObject shieldMesh;
+	float baseY, baseX, baseZ;
 
 	// Use this for initialization
 	void Start () {
-		
+		shieldMesh = GetComponentInParent<MeshRenderer> ().gameObject;
+		baseX = shieldMesh.transform.localScale.x;
+		baseY = shieldMesh.transform.localScale.y;
+		baseZ = shieldMesh.transform.localScale.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		shieldMesh.transform.localScale = new Vector3 (
+			baseX + Random.Range (-oscillationRate, oscillationRate),
+			baseY + Random.Range (-oscillationRate, oscillationRate),
+			baseZ
+		);
 	}
 
 
