5e5fca288b64424ffa1d22add4a1042df2c50985
lee-CS <john.flo.lee@gmail.com>
2015-03-30 05:33:33 -0400

    still working on bos

---
+++ Assets/Octo_Arm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Octo_Arm : MonoBehaviour {
+
+	public float			terminalVelocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+			
+		GameObject[] celestialBodies = GameObject.FindGameObjectsWithTag ("Planet");
+		
+
+		
+		if (rigidbody.velocity.magnitude > terminalVelocity) {
+			rigidbody.velocity = rigidbody.velocity.normalized * terminalVelocity;
+		}
+		else {
+			foreach(GameObject planet in celestialBodies){
+				Vector3 _force = PlanetPhysics.S.universalGravity (planet, this.gameObject);
+				rigidbody.AddForce(_force);
+			}
+		}
+
+
+	}
+}
