5742e1deee005e7e58e5caa3cc3b686557d05960
lee-CS <john.flo.lee@gmail.com>
2015-03-30 06:08:29 -0400

    giving up for now

--- Assets/Octo_Arm.cs
+++ Assets/Octo_Arm.cs
@@ -15,18 +15,22 @@ public class Octo_Arm : MonoBehaviour {
 	
 			
 		GameObject[] celestialBodies = GameObject.FindGameObjectsWithTag ("Planet");
-		
+
+		foreach(GameObject planet in celestialBodies){
+			Vector3 _force = boss_physics.S.universalGravity (planet, this.gameObject);
+			rigidbody2D.AddForce(_force);
+		}
 
 		
-		if (rigidbody.velocity.magnitude > terminalVelocity) {
-			rigidbody.velocity = rigidbody.velocity.normalized * terminalVelocity;
+		if (rigidbody2D.velocity.magnitude > terminalVelocity) {
+			rigidbody2D.velocity = rigidbody2D.velocity.normalized * terminalVelocity;
 		}
-		else {
-			foreach(GameObject planet in celestialBodies){
-				Vector3 _force = PlanetPhysics.S.universalGravity (planet, this.gameObject);
-				rigidbody.AddForce(_force);
-			}
-		}
+//		else {
+//			foreach(GameObject planet in celestialBodies){
+//				Vector3 _force = PlanetPhysics.S.universalGravity (planet, this.gameObject);
+//				rigidbody2D.AddForce(_force);
+//			}
+//		}
 
 
 	}
--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 1290
+                Build = 1334
 			};
 		}
 		public static VersionInfo UnityVersion()
