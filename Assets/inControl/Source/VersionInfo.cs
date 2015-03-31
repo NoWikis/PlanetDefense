790411f5bb55527dae531e7e4284c12b24f0299d
lee-CS <john.flo.lee@gmail.com>
2015-03-31 06:04:03 -0400

    fixed miners

--- Assets/inControl/Source/VersionInfo.cs
+++ Assets/inControl/Source/VersionInfo.cs
@@ -22,7 +22,7 @@ namespace InControl
 				Major = 1,
 				Minor = 4,
 				Patch = 3,
-                Build = 1415
+                Build = 1458
 			};
 		}
 		public static VersionInfo UnityVersion()
