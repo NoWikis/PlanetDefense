a99a13193d66db3f6580cbf5de4d3c822c834c81
jcorks <jcorkery@umich.edu>
2015-04-01 02:19:38 -0400

    Added objective and marker

---
+++ Assets/Scripts/level_manager/ObjectiveMarker.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveMarker : MonoBehaviour {
+	
+	
+	SpriteRenderer sprite;
+	GameObject planet;
+	GameObject objective;
+	public float limitLength = 20;
+	public float fadeBase = 1;
+	public float fadeFactor = 10;
+	public float zLayer = 0;
+	public float time = 0;
+
+	void Awake() {
+		sprite = GetComponentInChildren<SpriteRenderer> ();
+		objective = GameObject.FindGameObjectWithTag ("Objective");
+		//sprite.enabled = false;
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		planet = GameObject.FindGameObjectWithTag ("Planet");
+		
+		
+		
+		
+		//sprite.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (objective == null || planet == null) {
+			Destroy (gameObject);
+			return;
+		}
+		
+		//sprite.enabled = !(transform.parent.GetComponent<Renderer>().isVisible);
+		Vector3 newPos = objective.transform.position - planet.transform.position;
+		if (newPos.x > limitLength)
+			newPos.x = limitLength;
+		if (newPos.x < -limitLength)
+			newPos.x = -limitLength;
+		if (newPos.y < -limitLength) 
+			newPos.y = -limitLength;
+		if (newPos.y > limitLength)
+			newPos.y = limitLength;
+		
+		transform.rotation = Quaternion.Euler (
+			0, 
+			0, 
+			Util.getAngleVector(objective.transform.position, planet.transform.position) - 180
+			);
+		
+		
+		transform.position = new Vector3(newPos.x + planet.transform.position.x,
+		                                 newPos.y + planet.transform.position.y,
+		                                 transform.position.z);
+
+
+
+		
+		time += Time.deltaTime;
+		float alpha = .5f + .5f*Mathf.Sin(2*time);
+
+
+		sprite.color = new Color (
+			1f, 
+			1f, 
+			1f, 
+			alpha
+		);
+
+
+		//Debug.Log (alpha);
+
+	}
+}
